1a8725a6a53ffcfa86d5b7573c95b5f19cfc8e4e
Emiliano Acerbi <eacerbip@gmail.com>
2022-03-16 20:30:33 -0300

    pricing

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const hamburguer = document.querySelector('.header__ham')
 const mobileMenu = document.querySelector('.header__menu')
 
-const globalShadow = document.querySelector('.global-shadow')
-
-const modalCross = document.querySelector('.main__project__cross')
 const modalMenu = document.querySelector('.main__project')
 const modalButton = document.querySelector('.main__header__button')
+const modalCross = document.querySelector('.main__project__cross')
+const accordions = document.querySelectorAll('.main__project__card__acc')
+
+
+const radio = document.getElementsByName('pledge')
+
+
+for (let button of radio) {
+
+    button.addEventListener('click', () => {
+    
+        for(let acc of accordions) {
+            acc.classList.remove('active')
+        }
+
+        if(button.checked) {
+            if(button.value === 'no-reward') {
+                accordions[0].classList.add('active')
+            } 
+    
+            else if (button.value === 'bamboo'){
+                accordions[1].classList.add('active')
+            } 
+
+            else if (button.value === 'black') {
+                accordions[2].classList.add('active')
+            }
+        }
+    })
+    
+}
+
 
 let isDisplayed = false;
-
-let isModalDisplayed = false;
 
 const displayMenu = () => {
 
@@ -25,18 +52,13 @@ const displayMenu = () => {
 
 const openModal = () => {
     modalMenu.style.transform = 'scale(1) translateX(-50%)'    
-    // globalShadow.style.opacity = '.2'
 }  
 
 const closeModal = () => {
     modalMenu.style.transform = 'scale(0) translateX(-50%)'
-    // globalShadow.style.opacity = '0'
 } 
     
 
-
 hamburguer.addEventListener('click', displayMenu)
-
 modalButton.addEventListener('click', openModal)
-
 modalCross.addEventListener('click', closeModal)
