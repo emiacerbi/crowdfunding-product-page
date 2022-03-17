d9f3d90317f4d653be2126cf54555123c3b777ce
Emiliano Acerbi <eacerbip@gmail.com>
2022-03-16 22:29:14 -0300

    finished pricing functionality

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ const modalButton = document.querySelector('.main__header__button')
 const modalCross = document.querySelector('.main__project__cross')
 const accordions = document.querySelectorAll('.main__project__card__acc')
 
+const priceInputs = document.querySelectorAll('.main__project__card__acc__input')
+const priceButton = document.querySelectorAll('.main__project__card__acc__cont')
+
+const backedAmount = document.getElementById('backed-amount')
+const backersAmount = document.getElementById('backers-amount')
+const percentageBar = document.getElementById('progress-bar')
+
+let initialBackedAmount = 89914
+let initialBackers = 5007
+let initialPercentage = String(initialBackedAmount).slice(0, 2)
+
+
+// backedAmount.innerText = `$${initialBackedAmount}`
+
+
 
 const radio = document.getElementsByName('pledge')
 
@@ -15,7 +30,11 @@ for (let button of radio) {
     button.addEventListener('click', () => {
     
         for(let acc of accordions) {
-            acc.classList.remove('active')
+            acc.classList.remove('active')            
+        }
+
+        for (price of priceInputs) {
+            price.value = null
         }
 
         if(button.checked) {
@@ -57,8 +76,40 @@ const openModal = () => {
 const closeModal = () => {
     modalMenu.style.transform = 'scale(0) translateX(-50%)'
 } 
-    
+
+const getBackers = () => {
+    initialBackers++
+    backersAmount.innerText = `${initialBackers}`
+}
+
+const getPledgePrice = () => {
+    for (price of priceInputs) {
+        if (parseInt(price.value) > 0) {
+            initialBackedAmount += parseInt(price.value)
+            backedAmount.innerText = `$${initialBackedAmount}`
+
+            initialPercentage = String(initialBackedAmount).slice(0, 2)
+
+            if (initialBackedAmount > 100000) {
+                percentageBar.style.width = "100%"
+            } else {
+                percentageBar.style.width = `${initialPercentage}%`
+            }
+            
+            price.value = null
+
+            getBackers()
+        } 
+    }
+}
+
 
 hamburguer.addEventListener('click', displayMenu)
 modalButton.addEventListener('click', openModal)
 modalCross.addEventListener('click', closeModal)
+
+for (let pButton of priceButton) {
+    pButton.addEventListener('click', getPledgePrice)
+}
+
+console.log(parseInt('3'))
